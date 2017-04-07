20c5107082d198f5a597a8670d0b8ccf91c50632
Chaz Henricks <chazhenricks@gmail.com>
2017-04-07 10:13:08 -0500

    Started on project

---
+++ converter.js
@@ -0,0 +1,20 @@
+var convert = document.getElementById("convert");
+var clearButton = document.getElementById("clear");
+var textField = document.getElementsByName("temp");
+
+function toFahrenheit(){
+
+
+}
+
+function toCelsius(){
+
+	
+}
+
+function clearField(){
+	textField.value = "";
+}
+
+
+clearButton.onclick = console.log(document.getElementById("val1").value);
