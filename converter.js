35aa423320af8fb687b68d7c3f74f75e555e3645
Chaz Henricks <chazhenricks@gmail.com>
2017-04-08 13:54:24 -0500

    working temp converter

--- converter.js
+++ converter.js
@@ -1,20 +1,50 @@
 var convert = document.getElementById("convert");
-var clearButton = document.getElementById("clear");
-var textField = document.getElementsByName("temp");
+var clearButton = document.getElementById("resetForm");
 
+var newTemp="";
+//var tempField = document.getElementById("input-temp").value;
 function toFahrenheit(){
+  newTemp = (document.getElementById("input-temp").value * 9);
+  newTemp = (newTemp / 5);
+  newTemp = (newTemp + 32);
 
-
+  document.getElementById("newTemp").value = newTemp;
 }
 
 function toCelsius(){
 
-	
+  newTemp = (document.getElementById("input-temp").value - 32);
+  newTemp = (newTemp * 5);
+  newTemp = (newTemp / 9);
+
+  newTemp = Math.floor(newTemp);
+  if (newTemp < 0){
+    document.getElementById("newTemp").className += "cold";
+  } else if (newTemp > 32){
+    document.getElementById("newTemp").className += "hot";
+  } else {
+    document.getElementById("newTemp").className += "normal";
+  }
+
+  document.getElementById("newTemp").value = newTemp;
+  }
+
+
+function checkState(){
+  if (document.getElementById("f").checked){
+    toFahrenheit();
+  } else if (document.getElementById("c").checked){
+    toCelsius();
+  }
 }
 
-function clearField(){
-	textField.value = "";
+function clearField() {
+  document.getElementById("tempForm").reset();
 }
 
+clearButton.addEventListener("click", clearField);
+convert.addEventListener("click", checkState);
 
-clearButton.onclick = console.log(document.getElementById("val1").value);
+
+
+
